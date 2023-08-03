b727e9ab6023a135785ed895b8810c397e5ac2b6
Mahrukh khan <107424325+Suga7772@users.noreply.github.com>
2023-08-03 12:13:35 +0500

    add files

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+from bardapi import Bard
+
+app = Flask(__name__)
+token = 'ZQiv8vsJu0UHGwyNxlhsixkhDPbdJ2Z68iZecD_Ljt8zFFl2xyPvqjkd3XPeLVjqv3rBEQ.'
+bard = Bard(token=token)
+
+@app.route('/get_answer', methods=['POST'])
+def get_answer():
+    query = request.json.get('query')
+    response = bard.get_answer(query)['content']
+    return jsonify({'response': response})
+
+@app.route('/image_description', methods=['POST'])
+def image_description():
+    image_data = request.files.get('image')
+    if image_data:
+        image_bytes = image_data.read()
+        bard_answer = bard.ask_about_image('What is in the image ', image_bytes)
+        return jsonify({'description': bard_answer['content']})
+    else:
+        return jsonify({'error': 'No image provided.'}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
